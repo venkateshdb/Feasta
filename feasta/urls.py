419c3ad206ca26a523c99d5d2a68225d81298b1e
Umang-Thakur <61343294+Umang-Thakur@users.noreply.github.com>
2020-03-13 09:13:11 -0500

    Demo design for Login Page

--- feasta/urls.py
+++ feasta/urls.py
@@ -22,6 +22,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+    path('login/',include('login.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/', include('mess.urls')),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
