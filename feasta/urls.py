a5fcf09e57aae5dd0d7cfbc31a89db80a34f1292
Pavankumar Maurya <pavankumarmaurya1999@gmail.com>
2020-03-15 14:01:24 +0530

    Base Page Added

--- feasta/urls.py
+++ feasta/urls.py
@@ -22,11 +22,12 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('login/',include('login.urls')),
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('mess.urls')),
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('',include('mess.urls')),
+   path('login/',include('login.urls')),
+   path('admin/', admin.site.urls),
+   path('api/v1/', include('mess.urls')),
+   url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
